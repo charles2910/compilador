a9e1959efcd3b0879b336d2fe1835c32200b5828
Carlos Henrique Lima Melara <charlesmelara@outlook.com>
2020-06-17 12:18:44 -0300

    Att do protótipo da função `get_token`

--- analisador_lexico.h
+++ analisador_lexico.h
@@ -39,6 +39,8 @@
 #define NUM_INTEIRO			36
 #define NUM_REAL			37
 
+#include "hashmap.h"
+
 typedef struct {
 	char * string;
 	char * token;
@@ -48,38 +50,38 @@ typedef struct {
 /**
  * Função inclui no hashmap as palavras reservadas. Retorna 0 em sucesso.
  */
-int populate_hashmap(map_t )
+int populate_hashmap(map_t );
 
 /**
  * Função que carrega um arquivo fonte passado como parâmetro.
  */
-int load_file(char * , char * )
+int load_file(char * , char * );
 
 /**
  * Função que lê e retorna uma linha do arquivo aberto.
  */
-int fill_buffer(FILE * , char * )
+int fill_buffer(FILE * , char * );
 
 /**
  * Função que recebe o estado atual e o símbolo lido, calcula a transição
  * e retorna o novo estado.
  */
-int get_next_state(int , char )
+int get_next_state(int , char );
 
-par_token * get_par_token(char * )
+par_token * get_par_token(char * );
 
-char * new_buffer()
+char * new_buffer();
 
-int consumir(int * , int )
+int consumir(int * , int );
 
-char * get_token(char * , char * , int )
+par_token * get_token(char * , char ** , int );
 
-int is_error(int )
+int is_error(int );
 
-int relata_erro(int , char * , int )
+int relata_erro(int , char * , int );
 
-int verify_rollback_state(int )
+int verify_rollback_state(int );
 
-int is_final_state(int )
+int is_final_state(int );
 
-int print_token(par_token * )
+int print_token(par_token * );
